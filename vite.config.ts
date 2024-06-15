b1359a5d25e59e7a08c01f2e260fc2b51fefc81f
DinoStark <15190304050@163.com>
2024-06-15 09:37:43 +0800

    Enable dynamic target by environment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,30 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    envDir: "./env",
-    plugins: [react()],
-    server: {
-        host: '127.0.0.1',
-        port: 3300,
-        open: false,
-        cors: true,
-        proxy:
-            {
-                "/api":
-                    {
-                        target: "http://localhost:19332",
-                        changeOrigin: true,
-                        rewrite: (path) => path.replace(/^\/api/, '')
-                    }
-            }
+export default defineConfig(({mode}) =>
+{
+    // Load environment configurations and set it as proxy target.
+    const env: Record<string, string> = loadEnv(mode, "./env");
+
+    return {
+        envDir: "./env",
+        plugins: [react()],
+        server: {
+            host: '127.0.0.1',
+            port: 3300,
+            open: false,
+            cors: true,
+            proxy:
+                {
+                    "/api":
+                        {
+                            // target: "http://localhost:19332",
+                            target: env.VITE_API_URL,
+                            changeOrigin: true,
+                            rewrite: (path) => path.replace(/^\/api/, "")
+                        }
+                }
+        }
     }
 })
